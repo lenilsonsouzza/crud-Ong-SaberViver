710b49d6e86d9689d76aedd900a6ee5205902f88
lenilsonsouzza <lenilsonsouzza@gmail.com>
2025-10-27 12:54:21 -0300

    alterando url do front no beck2

--- src/main/java/saberViver/com/appSaberviver/infra/seguranca/ConfiguracaoSeguranca.java
+++ src/main/java/saberViver/com/appSaberviver/infra/seguranca/ConfiguracaoSeguranca.java
@@ -77,7 +77,7 @@ public class ConfiguracaoSeguranca {
                 "http://127.0.0.1:5500",
                 "http://localhost:5500",
                 "http://localhost:5173",
-                "https://vercel.com/lenilsonsouzzas-projects/ong-sabeviver",
+                "https://ong-sabeviver.vercel.app",
                 "https://ong-saberviver.up.railway.app"
         ));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
